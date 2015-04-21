10263d121c3bc7bb30ea575badf9801026bf4ce2
Kefan Jian <kefanjian@gmail.com>
2015-04-21 15:13:53 +0800

    scan

---
+++ UIExample/UIExample/ScanCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  ScanCollectionViewController.h
+//  UIExample
+//
+//  Created by Kefan Jian on 2015/4/21.
+//  Copyright (c) 2015年 Joseph Liao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScanCollectionViewController : UICollectionViewController
+
+@end
