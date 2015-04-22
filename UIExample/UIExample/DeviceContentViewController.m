eb6ae83dddac654dd5486d1b39f535b9aca00231
joseph <joseph.liao@gigabyte.com>
2015-04-22 10:21:33 +0800

    device content view -- storyboard down

--- UIExample/UIExample/DeviceContentViewController.m
+++ UIExample/UIExample/DeviceContentViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "DeviceContentViewController.h"
+#import "VOSegmentedControl.h"
 
 @interface DeviceContentViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *theTitle;
+
+
 
 @end
 
@@ -19,12 +24,66 @@
     // Do any additional setup after loading the view.
     
     NSLog(@"Now Is %d Device",self.device);
+    
+    VOSegmentedControl *segctrl1 = [[VOSegmentedControl alloc] initWithSegments:
+                                    @[@{@"text": @"Cat",@"image": @"torah-32"},
+                                      @{@"text": @"Kid",@"image": @"torah-32"},
+                                      @{@"text": @"Run",@"image": @"torah-32"},
+                                      @{@"text": @"重訓",@"image": @"torah-32"},
+                                      @{@"text": @"中控",@"image": @"torah-32"},
+                                      @{@"text": @"開冰箱",@"image": @"torah-32"}]];
+    
+    segctrl1.contentStyle = VOContentStyleImageTop;
+    segctrl1.indicatorStyle = VOSegCtrlIndicatorStyleBox;
+    segctrl1.animationType = VOSegCtrlAnimationTypeSmooth;
+    segctrl1.backgroundColor = [UIColor groupTableViewBackgroundColor];
+    segctrl1.allowNoSelection = NO;
+    segctrl1.frame = CGRectMake(0, self.view.frame.size.height-self.view.frame.size.height/8,
+                                self.view.frame.size.width, self.view.frame.size.height/8);
+//    segctrl1.selectedTextFont = [UIFont systemFontOfSize:15];
+    segctrl1.indicatorThickness = 1.5;
+    segctrl1.indicatorCornerRadius = 6;
+    segctrl1.selectedIndicatorColor = [UIColor blackColor];
+    
+    segctrl1.tag = 1;
+    segctrl1.selectedSegmentIndex = self.device;
+    [self.view addSubview:segctrl1];
+    [segctrl1 setIndexChangeBlock:^(NSInteger index) {
+        NSLog(@"1: block --> %@", @(index));
+        
+        
+        
+        
+    }];
+    [segctrl1 addTarget:self action:@selector(segmentCtrlValuechange:)
+       forControlEvents:UIControlEventValueChanged];
+
+    
+}
+
+- (void)segmentCtrlValuechange: (VOSegmentedControl *)segmentCtrl{
+    NSLog(@"%@: value --> %@",@(segmentCtrl.tag), @(segmentCtrl.selectedSegmentIndex));
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+#pragma -
+#pragma - IBActioin
+- (IBAction)editButton:(UIButton *)sender {
+}
+
+- (IBAction)backButton:(UIButton *)sender {
+    
+    [self dismissViewControllerAnimated:YES completion:^{
+        //
+    }];
+    
+}
+
 
 /*
 #pragma mark - Navigation
